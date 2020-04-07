0e215ebeeefe13eb05939529eca5fca7666e2889
Braeden <bmw54@duke.edu>
2020-04-07 00:02:24 -0400

    Implemented empty methods of the API 'DataReader' as a skeleton; Copied some code into one of the methods, will change later; Created a very simple Thumbnail to be used later

--- src/ooga/data/OogaDataReader.java
+++ src/ooga/data/OogaDataReader.java
@@ -1,4 +1,86 @@
 package ooga.data;
 
-public class OogaDataReader {
+import ooga.OogaDataException;
+import ooga.game.Game;
+
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+
+public class OogaDataReader implements DataReader{
+
+    @Override
+    public List<String> getThumbnails(String folderPath) throws OogaDataException {
+        return null;
+    }
+
+    @Override
+    public List<String> getGameFiles(String folderPath) throws OogaDataException {
+        try {
+            File fXmlFile = new File("/Users/mkyong/staff.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fXmlFile);
+
+            //optional, but recommended
+            //read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+            doc.getDocumentElement().normalize();
+
+            System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+
+            NodeList nList = doc.getElementsByTagName("staff");
+
+            System.out.println("----------------------------");
+
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+
+                Node nNode = nList.item(temp);
+
+                System.out.println("\nCurrent Element :" + nNode.getNodeName());
+
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+                    Element eElement = (Element) nNode;
+
+                    System.out.println("Staff id : " + eElement.getAttribute("id"));
+                    System.out.println("First Name : " + eElement.getElementsByTagName("firstname").item(0).getTextContent());
+                    System.out.println("Last Name : " + eElement.getElementsByTagName("lastname").item(0).getTextContent());
+                    System.out.println("Nick Name : " + eElement.getElementsByTagName("nickname").item(0).getTextContent());
+                    System.out.println("Salary : " + eElement.getElementsByTagName("salary").item(0).getTextContent());
+
+                }
+            }
+        } catch (Exception e) {
+            // TODO: This ^v is gross get rid of it :) (written by Braeden to Braeden)
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    public Game loadGame(String filePath) throws OogaDataException {
+        return null;
+    }
+
+    @Override
+    public void saveGameState(String filePath) throws OogaDataException {
+
+    }
+
+    @Override
+    public Game loadGameState(String filePath) throws OogaDataException {
+        return null;
+    }
+
+    @Override
+    public String getEntityImage(String entityName, String gameFile) throws OogaDataException {
+        return null;
+    }
 }
---
+++ src/ooga/data/Thumbnail.java
@@ -0,0 +1,17 @@
+package ooga.data;
+
+import java.awt.*;
+
+/**
+ * This class encapsulates the initial representation of a Game on the home screen. It is everything the DataReader needs
+ * to give initially including but not limited to the thumbnail image, the game's title, and the game's description.
+ */
+public class Thumbnail {
+    private Image myImage;
+    private String myTitle;
+    private String myDescription;
+
+    public Thumbnail(String imageFileName, String title, String myDescription){
+
+    }
+}
