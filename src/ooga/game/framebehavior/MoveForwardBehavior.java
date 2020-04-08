5edc8cda849cd023aa376261f538e39abfaa36e3
Doherty Guirand <dg211@duke.edu>
2020-04-07 21:26:49 -0400

    changed the MoveForwardBehavior class to reflect API changes

--- src/ooga/game/framebehavior/MoveForwardBehavior.java
+++ src/ooga/game/framebehavior/MoveForwardBehavior.java
@@ -2,28 +2,26 @@ package ooga.game.framebehavior;
 
 import ooga.EntityAPI;
 import ooga.MovementBehavior;
+import ooga.data.Entity;
 
 public class MoveForwardBehavior implements MovementBehavior {
 
-  public static final double X_MOVE_PER_SECOND = 10;
-  public static final double Y_MOVE_PER_SECOND = 0;
-  private EntityAPI myEntity;
 
-  public MoveForwardBehavior(EntityAPI e) {
-    setTarget(e);
+  private EntityAPI myEntity;
+  private double xMovePerFrame;
+  private double yMovePerFrame;
+
+  public MoveForwardBehavior(double xmove, double ymove) {
+    xMovePerFrame = xmove;
+    yMovePerFrame = ymove;
   }
 
   public MoveForwardBehavior() {
-    this(null);
+    this(0,0);
   }
 
   @Override
-  public void setTarget(EntityAPI e) {
-    myEntity = e;
-  }
-
-  @Override
-  public void doMovementUpdate(double elapsedTime) {
-    myEntity.move(X_MOVE_PER_SECOND,Y_MOVE_PER_SECOND);
+  public void doMovementUpdate(double elapsedTime, Entity subject) {
+    subject.move(xMovePerFrame * elapsedTime,yMovePerFrame * elapsedTime);
   }
 }
