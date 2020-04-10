8ba40e5cb13357f599f98e15d628cb702ba17eed
Braeden <bmw54@duke.edu>
2020-04-09 20:03:16 -0400

    made a working getBasicGameInfo

--- tests/ooga/data/DataReaderTest.java
+++ tests/ooga/data/DataReaderTest.java
@@ -12,16 +12,34 @@ import java.util.List;
  * @author braedenward
  */
 public class DataReaderTest {
+    private OogaDataReader testDataReader = new OogaDataReader();
+    private String GAME_NAME = "Super Mario Bros";
+    private String ID = "1";
 
     @Test
-    public void runTest(){
-        OogaDataReader testDataReader = new OogaDataReader();
+    public void testGetThumbnails(){
         List<Thumbnail> thumbnailList = testDataReader.getThumbnails();
-        for(Thumbnail t : thumbnailList){
+        for (Thumbnail t : thumbnailList) {
             System.out.println(String.format("Title: %s \nDescription: %s \nImage: %s\n", t.getTitle(), t.getDescription(), t.getImageFile()));
         }
+    }
+    @Test
+    public void testGetBasicGameInfo(){
+        List<String> stringList = null;
         try {
-            testDataReader.loadLevel("Super Mario Bros", "1");
+            stringList = testDataReader.getBasicGameInfo("Super Mario Bros");
+        } catch (OogaDataException e) {
+            // TODO: Fix this, Braeden
+            System.out.println("Test Failed");
+            e.printStackTrace();
+        }
+        System.out.println("List of Level IDs for " + GAME_NAME + ": " + stringList + "\n");
+    }
+
+    @Test
+    public void testLoadLevel(){
+        try {
+            testDataReader.loadLevel(GAME_NAME, ID);
         } catch (OogaDataException e) {
             // TODO: Fix this, Braeden
             System.out.println("Test Failed");
