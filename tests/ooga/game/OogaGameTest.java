cebefa11f897ecdf0ea3d1fc435537c7f6f9df92
Sam Thompson <stt13@duke.edu>
2020-04-17 01:15:52 -0400

    fleshed out some game tests, expanded level tests

--- tests/ooga/game/OogaGameTest.java
+++ tests/ooga/game/OogaGameTest.java
@@ -2,6 +2,7 @@ package ooga.game;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
@@ -12,35 +13,37 @@ import ooga.Entity;
 import ooga.OogaDataException;
 import ooga.UserInputListener;
 import ooga.data.ImageEntity;
+import ooga.data.OogaDataReader;
+import ooga.data.OogaEntity;
 import ooga.game.behaviors.asyncbehavior.DestroySelfBehavior;
 import ooga.game.behaviors.framebehavior.MoveForwardBehavior;
 import ooga.game.behaviors.inputbehavior.JumpBehavior;
 import org.junit.jupiter.api.Test;
 
-public class GameTest {
-
-  @Test
-  void testGameInitialize() throws OogaDataException {
-    Game loadTest = ControlsTestGameCreation.getGame();
-    assertTrue(loadTest.getEntities().size() > 0);
-
-  }
+public class OogaGameTest {
 
   @Test
   void testDoUpdateLoop() {
-    Entity moveForwardEntity = new ImageEntity();
-//    moveForwardEntity.setMovementBehaviors(List.of(new MoveForwardBehavior(10.0,20.0)));
-    Level level = new OogaLevel(List.of(moveForwardEntity));
-    OogaGame game = new OogaGame(level);
-    double expectedX = 0;
-    double expectedY = 0;
-    for (int i = 0; i < 5; i ++) {
-      double elapsedTime = (0.1 * (i));
-      expectedX += elapsedTime * 10.0;
-      expectedY += elapsedTime * 20.0;
-      game.doGameStep(elapsedTime);
+    final double FRICTION_ACCELERATION = OogaEntity.FRICTION_ACCELERATION;
+    try {
+      OogaGame game = new OogaGame("Super Mario Bros", new OogaDataReader("data/games-library"));
+      double expectedX = 0;
+      double expectedY = 0;
       Entity targetEntity = game.getEntities().get(0);
-      assertEquals(List.of(expectedX,expectedY),targetEntity.getPosition());
+      targetEntity.setVelocity(1.0,2.0);
+      targetEntity.setMovementBehaviors(new ArrayList<>());
+      for (int i = 0; i < 5; i ++) {
+        double elapsedTime = (0.1 * (i));
+        expectedX += elapsedTime * (1.0 - 2.0 * FRICTION_ACCELERATION);
+        expectedY += elapsedTime * (2.0 - 2.0 * FRICTION_ACCELERATION);
+        game.doGameStep(elapsedTime);
+        assertEquals((Double)expectedX,targetEntity.getPosition().get(0));
+        assertEquals((Double)expectedY,targetEntity.getPosition().get(1));
+//        assertEquals(List.of(expectedX,expectedY),targetEntity.getPosition());
+      }
+    }
+    catch (OogaDataException e) {
+      fail();
     }
   }
 
---
+++ tests/ooga/game/OogaLevelTest.java
@@ -0,0 +1,53 @@
+package ooga.game;
+
+import static org.junit.Assert.assertEquals;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import ooga.Entity;
+import ooga.data.ImageEntity;
+import org.junit.jupiter.api.Test;
+
+public class OogaLevelTest {
+
+  public static final int NUM_ASSERTS = 10;
+
+  @Test
+  void testRemoveEntity() {
+    Entity removable = new ImageEntity();
+    Level level = new OogaLevel(List.of(removable));
+    assertEquals(1,level.getEntities().size());
+    level.removeEntity(removable);
+    assertEquals(0,level.getEntities().size());
+  }
+
+  @Test
+  void testAddEntity() {
+    Entity addable = new ImageEntity("add");
+    Level level = new OogaLevel(new ArrayList<>());
+    assertEquals(0,level.getEntities().size());
+    level.addEntity(addable);
+    assertEquals(1,level.getEntities().size());
+    assertEquals(addable.getName(),level.getEntities().get(0).getName());
+    for (int i = 0; i < NUM_ASSERTS-1; i ++) {
+      level.addEntity(new ImageEntity());
+      assertEquals(i+2,level.getEntities().size());
+    }
+    List<Entity> testList = List.of(new ImageEntity(), new ImageEntity());
+    level.addEntities(testList);
+    assertEquals(NUM_ASSERTS+2,level.getEntities().size());
+  }
+
+  @Test
+  void testLevelId() {
+    Level level = new OogaLevel(new ArrayList<>());
+    byte [] rand = new byte[10];
+    new Random().nextBytes(rand);
+    String randString = new String(rand, StandardCharsets.UTF_8);
+    level.setNextLevelID(randString);
+    assertEquals(randString,level.nextLevelID());
+  }
+}
