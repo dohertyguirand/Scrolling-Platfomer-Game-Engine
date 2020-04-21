76a4ebe49b0ed3d92153fc4583c0c84de3327032
Cary Shindell <css57@duke.edu>
2020-04-21 15:59:22 -0400

    tiny change

--- src/ooga/game/behaviors/asyncbehavior/RunIntoTerrain.java
+++ src/ooga/game/behaviors/asyncbehavior/RunIntoTerrain.java
@@ -4,8 +4,9 @@ import java.util.List;
 import java.util.Map;
 import ooga.game.GameInternal;
 import ooga.Entity;
+import ooga.game.behaviors.CollisionEffect;
 
-public abstract class RunIntoTerrain extends QuadDirectionCollision {
+public abstract class RunIntoTerrain implements CollisionEffect {
 
   public RunIntoTerrain(List<String> args) {
     //arguments have no effect on this behavior
