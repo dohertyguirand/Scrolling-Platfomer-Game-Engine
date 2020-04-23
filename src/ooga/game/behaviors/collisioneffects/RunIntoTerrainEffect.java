716007364600728e2a5b45557ed1cd7579eb7d43
Cary Shindell <css57@duke.edu>
2020-04-22 21:48:59 -0400

    small changes

--- src/ooga/game/behaviors/collisioneffects/RunIntoTerrainEffect.java
+++ src/ooga/game/behaviors/collisioneffects/RunIntoTerrainEffect.java
@@ -7,8 +7,6 @@ import ooga.game.behaviors.TimeDelayedEffect;
 public abstract class RunIntoTerrainEffect extends TimeDelayedEffect {
 
   public RunIntoTerrainEffect(List<String> args) {
-    if(args.size() > 0){
-      setTimeDelay(args.get(0));
-    }
+    super(args);
   }
 }
